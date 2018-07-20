89c6ad589941236d6c30b1f5b35c7aa545a34ead
Kinkuraj Sahoo <kinkuraj@users.noreply.github.com>
2018-07-20 08:00:20 +0200

    Removed API key

--- src/main/java/com/weatherbot/api/bot/WeatherService.java
+++ src/main/java/com/weatherbot/api/bot/WeatherService.java
@@ -9,8 +9,8 @@ import com.weatherbot.api.pojo.Weather;
 public class WeatherService {
 
 	public static final String DEFAULT_REPLY="Whoops this is embarrassing, Enter city name or pin code. e.g. Chennai or 600119";
-	private static final String URL ="http://api.openweathermap.org/data/2.5/weather?q=##,IN&&units=metric&appid=3f9c5b805c57433d13ae480098c16c5e";
-	private static final String URL_ZIP ="http://api.openweathermap.org/data/2.5/weather?zip=##,IN&&units=metric&appid=3f9c5b805c57433d13ae480098c16c5e";
+	private static final String URL ="http://api.openweathermap.org/data/2.5/weather?q=##,IN&&units=metric&appid=<API-Key>";
+	private static final String URL_ZIP ="http://api.openweathermap.org/data/2.5/weather?zip=##,IN&&units=metric&appid=<API-Key>";
 	public static String getTemperature(String zipCode) {
 		BotLogger.info("WeatherService", "Weather details requested for pin code: " + zipCode);
 		String weatherData = Consumer.requestContent(Consumer.getUrl(URL_ZIP,zipCode));
@@ -24,7 +24,7 @@ public class WeatherService {
 
 	private static StringBuilder createAText(Weather weather) {
 		StringBuilder textMsg = new StringBuilder(weather.getName());
-		textMsg.append(":\n Temp: "+ weather.getMain().getTemp().intValue() +"°C");
+		textMsg.append(":\n Temp: "+ weather.getMain().getTemp().intValue() +"Â°C");
 		textMsg.append("\n Humidity: "+ weather.getMain().getHumidity() +"%");
 		textMsg.append("\n Wind: "+ weather.getWind().getSpeed() +"m/s");
 		return textMsg;
